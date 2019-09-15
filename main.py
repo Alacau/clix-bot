243c0dbf8512b4eba2313eefd40980c2e306e50d
Alan Cao <alancao1@yahoo.com>
2019-09-14 21:14:10 -0700

    day 1

---
+++ main.py
@@ -0,0 +1,13 @@
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = '.')
+
+@client.event
+async def on_ready():
+    print('Bot is ready')
+
+@client.command()
+async def nuke(ctx):
+
+client.run('TOKEN')
