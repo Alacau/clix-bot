8ff4736655ebf9054bd67bdc765a41d29abc05c1
Alan Cao <alancao1@yahoo.com>
2019-09-15 20:41:45 -0700

    day2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import glob
 
 client = commands.Bot(command_prefix = '.')
 
@@ -8,7 +9,17 @@ async def on_ready():
     print('Bot is ready')
 
 @client.command()
-async def ping(ctx):
-    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+async def test(ctx, *, kwarg):
+    for image_file in glob.glob('png/*'):
+        file_name = image_file.split('/')[-1]
+        if file_name.startswith(f'{kwarg.lower()}'):
+            await ctx.send(embed = discord.Embed(title = 'Title'))
+
+@client.command()
+async def pug(ctx, *, kwarg):
+    for image_file in glob.glob('png/*'):
+        file_name = image_file.split('/')[-1]
+        if file_name.startswith(f'{kwarg.lower()}'):
+            await ctx.send(file = discord.File(f'{image_file}'))
 
 client.run('TOKEN')
