da7d6b554ed30da157a39a13b6198bcc6c1cb0e9
Alan Cao <alancao1@yahoo.com>
2019-09-18 08:58:27 -0700

    day 5

--- main.py
+++ main.py
@@ -11,45 +11,51 @@ client = commands.Bot(command_prefix = get_prefix)
 
 @client.event
 async def on_ready():
-    print('Bot is ready')
+    print('READY.')
 
 #TESTING AREA
 @client.command()
-async def test(ctx, *, kwarg):
-    pass
+async def test(ctx, *, frame):
+    with open('test.json', 'r') as h:
+        warframes = json.load(h)
+    for item in warframes:
+        for key in item:
+            if key == frame:
+                print(item[frame][0])
 #TESTING AREA
 
 #Core to organize into cog: frame, nuke, tricap,
 @client.command()
 async def frame(ctx, *, frame):
-
+    frame = frame.lower()
     with open('warframes.json', 'r') as f:
         warframe = json.load(f)
+
+    with open('warframe_img.json', 'r') as g:
+        img = json.load(g)
 
     build = warframe[frame][0].split('/')
     build_file = build[-1]
     build_name = build_file[:-4]
 
-    for value in range(len(warframe[frame])):
-        build = warframe[frame][value].split('/')
-        build_file = build[-1]
-        build_name = build_file[:-4]
-        print(build_name)
-
     embed = discord.Embed(
-    title = f'{frame.capitalize()}',
     description = '',
     color = discord.Color.teal()
     )
+    embed.set_author(name = build_name.upper(), icon_url = img[frame])
     embed.set_image(url = f'{warframe[frame][0]}')
-    embed.set_footer(text = '')
+    embed.set_thumbnail(url = img[frame])
+    embed.add_field(name = 'Warframe', value = frame.capitalize())
+    embed.set_footer(text = 'Got build suggestions? Send them to: EXAMPLE EMAIL', icon_url = img['choobi'])
 
     if len(warframe[frame]) > 1:
         for value in range(len(warframe[frame])):
             build = warframe[frame][value].split('/')
             build_file = build[-1]
             build_name = build_file[:-4]
+            embed.set_author(name = f'{build_name.upper()}', icon_url = f'{img[frame]}')
             embed.set_image(url = f'{warframe[frame][value]}')
+            embed.set_thumbnail(url = img[frame])
             await ctx.send(embed = embed)
     else:
         await ctx.send(embed = embed)
@@ -91,6 +97,5 @@ async def prefix(ctx, prefix):
         json.dump(prefixes, f, indent = 4)
 
     await ctx.send(embed = embed)
-
 
 client.run('TOKEN')
